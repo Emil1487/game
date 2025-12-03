06ff1d2f599632f97896a80299f96deda469861c
Emil2001 <serazdinovemil@gmail.com>
2025-12-03 18:40:59 +0400

    Update main_10.py

--- main_10.py
+++ main_10.py
@@ -13,6 +13,7 @@ DARK_RED = (209, 0, 0)
 
 FPS = 60
 
+
 pg.init()
 sc = pg.display.set_mode((W, H))
 pg.display.set_caption("car")
@@ -22,14 +23,27 @@ clock = pg.time.Clock()
 sc.fill(WHITE)
 
 class Player:
-
     def __init__(self):
+        self.pov = 90
         self.size = 10
         self.speed = 5
-        self.surf = pg.image.load('car.png')
+        self.surf = pg.image.load('images/car.png')
         self.rect = self.surf.get_rect(center=(W / 2, H / 2))
 
-    def move(self, dx=0, dy=0):
+    def move(self, dx=0, dy=0, povorot=90):
+        if povorot != self.pov:
+            if self.pov < povorot:
+                pov = self.pov
+                while pov != povorot:
+                    self.surf = pg.transform.rotate(self.surf, 90)
+                    pov += 90
+                self.pov = povorot
+            elif self.pov > povorot:
+                pov = self.pov
+                while pov != povorot:
+                    self.surf = pg.transform.rotate(self.surf, -90)
+                    pov -= 90
+                self.pov = povorot
         if (self.rect.left + dx * self.speed) > 0 and (self.rect.right + dx * self.speed) < W:
             self.rect.x += dx * self.speed
         if (self.rect.top + dy * self.speed) > 0 and (self.rect.bottom + dy * self.speed) < H:
@@ -48,13 +62,13 @@ while True:
 
     keys = pg.key.get_pressed()
     if keys[pg.K_LEFT]:
-        car.move(dx=-1)
-    if keys[pg.K_RIGHT]:
-        car.move(dx=1)
-    if keys[pg.K_UP]:
-        car.move(dy=-1)
-    if keys[pg.K_DOWN]:
-        car.move(dy=1)
+        car.move(dx=-1, povorot=180)
+    elif keys[pg.K_RIGHT]:
+        car.move(dx=1, povorot=360)
+    elif keys[pg.K_UP]:
+        car.move(dy=-1, povorot=90)
+    elif keys[pg.K_DOWN]:
+        car.move(dy=1, povorot=270)
 
     sc.fill(WHITE)
     car.draw()
